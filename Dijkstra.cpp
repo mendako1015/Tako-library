09018d5cb78eec0053dd436c987d6f56f46513f4
Chiharu.H <h1000sunny51@gmail.com>
2020-07-25 07:23:31 +0900

    updated Dijkstra.cpp

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,17 +1,10 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define cost first
-#define to second
-typedef pair<int, int> P;
-#define INF INT_MAX
-
 const int MAX_V = 100010;
 
-vector<P> G[MAX_V];
+vector<P> graph[MAX_V];
 int dist[MAX_V];
 int V; // 頂点数
 
-void dijikstra(int s) {
+void dijkstra(int s) {
 	priority_queue< P, vector<P>, greater<P> > que;
 	fill(dist, dist + V, INF);
 	dist[s] = 0;
@@ -21,7 +14,7 @@ void dijikstra(int s) {
 		P p = que.top(); que.pop();
 		int next = p.to;
 		if(dist[next] < p.cost) continue;
-		for(P e : G[next]) {
+		for(P e : graph[next]) {
 			if(dist[e.to] > dist[next] + e.cost) {
 				dist[e.to] = dist[next] + e.cost;
 				que.push(P(dist[e.to], e.to));
