b991d1e2cb55f96368a6a24bd902d753f506b88d
mendako1015 <h1000sunny51@icloud.com>
2020-12-07 06:47:45 +0900

    Add UnionFiind test

---
+++ Test/UnionFind.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+
+#include <bits/stdc++.h>
+using namespace std;
+#include "../DataStructure/UnionFind.cpp"
+
+int main() {
+	int n, q;
+	cin >> n >> q;
+	UnionFind<int> ut(n);
+	for(int i = 0; i < q; i++) {
+		int t, u, v;
+		cin >> t >> u >> v;
+		if(t == 0) ut.unite(u, v);
+		else cout << ut.same(u, v) << endl;
+	}
+	return 0;
+}
