338d57d3d5d046cb9f806ada9d074612431d869a
mendako1015 <h1000sunny51@icloud.com>
2020-12-15 01:25:57 +0900

    add SegmentTree test

---
+++ Test/SegmentTree-PointAddRangeSum.test.cpp
@@ -0,0 +1,26 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_sum"
+
+#include <bits/stdc++.h>
+using namespace std;
+#include "../DataStructure/SegmentTree.cpp"
+typedef long long ll;
+
+int main() {
+	int n, q;
+    cin >> n >> q;
+    SegmentTree<ll> segtree(n, 0,
+        [](ll a, ll b) { return a + b; },
+        [](ll a, ll b) { return b; });
+    for(int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        segtree.update(i, a);
+    }
+    while(q--) {
+        int cmd, x, y;
+		cin >> cmd >> x >> y;
+		if(cmd == 0) segtree.update(x, y);
+		else cout << segtree.get_interval(x, y) << endl;
+	}
+	return 0;
+}
