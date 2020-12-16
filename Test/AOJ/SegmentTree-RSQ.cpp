674a8773486ffb5d0585844b45d8a29036b1e6fc
mendako1015 <h1000sunny51@icloud.com>
2020-12-16 18:47:37 +0900

    added segtree test (RSQ)

---
+++ Test/AOJ/SegmentTree-RSQ.cpp
@@ -0,0 +1,27 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/DSL_2_B"
+
+#include <bits/stdc++.h>
+using namespace std;
+#include "../../DataStructure/SegmentTree.cpp"
+typedef long long ll;
+
+int main() {
+	ll n, q;
+	cin >> n >> q;
+	SegmentTree<ll> segtree(n+10, true, 0, [](ll x, ll y){ return x + y; }, [](ll x, ll y){ return x + y; } );
+	segtree.range_update(0, n, 0);
+	while(q--) {
+		int com;
+		cin >> com;
+		if(com == 0) {
+			ll x, y, z;
+			cin >> x >> y;
+			segtree.point_update(--x, y);
+		} else {
+			ll x, y;
+			cin >> x >> y;
+			cout << segtree.get_interval(--x, y) << endl;
+		}
+	}
+	return 0;
+}
