45907d64bb5c8b04767bc3a86efd8e4c38fcf6ed
mendako1015 <h1000sunny51@icloud.com>
2020-12-16 19:01:41 +0900

    hogehoge

--- Test/AOJ/SegmentTree-RSQ.cpp
+++ Test/AOJ/SegmentTree-RSQ.cpp
@@ -6,22 +6,22 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-	ll n, q;
-	cin >> n >> q;
-	SegmentTree<ll> segtree(n+10, true, 0, [](ll x, ll y){ return x + y; }, [](ll x, ll y){ return x + y; } );
-	segtree.range_update(0, n, 0);
-	while(q--) {
-		int com;
-		cin >> com;
-		if(com == 0) {
-			ll x, y, z;
-			cin >> x >> y;
-			segtree.point_update(--x, y);
-		} else {
-			ll x, y;
-			cin >> x >> y;
-			cout << segtree.get_interval(x, y) << endl;
-		}
-	}
+	// ll n, q;
+	// cin >> n >> q;
+	// SegmentTree<ll> segtree(n+10, true, 0, [](ll x, ll y){ return x + y; }, [](ll x, ll y){ return x + y; } );
+	// segtree.range_update(0, n, 0);
+	// while(q--) {
+	// 	int com;
+	// 	cin >> com;
+	// 	if(com == 0) {
+	// 		ll x, y, z;
+	// 		cin >> x >> y;
+	// 		segtree.point_update(--x, y);
+	// 	} else {
+	// 		ll x, y;
+	// 		cin >> x >> y;
+	// 		cout << segtree.get_interval(x, y) << endl;
+	// 	}
+	// }
 	return 0;
 }
