4b9e13e94ba6a696b2209dd6848211d81e164f94
mendako1015 <h1000sunny51@icloud.com>
2020-12-30 21:32:37 +0900

    add RollingHash test

--- Test/AOJ/RollingHash.test.cpp
+++ Test/AOJ/RollingHash.test.cpp
@@ -9,9 +9,7 @@ int main() {
 	string t, p;
 	cin >> t >> p;
 	if(t.size() < p.size()) return 0;
-	const ll MOD = 1e9+7;
-	const ll BASE = 1777771;
-	RollingHash<ll, MOD, BASE> rt(t), rp(p);
+	RollingHash<ll, 1000000007, 1777771> rt(t), rp(p);
 	for(int i = 0; i < t.size() - p.size() + 1; i++) {
 		if(rt.get(i, i + p.size()) == rp) cout << i << endl;
 	}
