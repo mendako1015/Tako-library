6daea1a3b8f8ecdf02c9d9ee086039f3b557b824
mendako1015 <h1000sunny51@icloud.com>
2020-12-30 21:45:34 +0900

    changed RollingHash test

---
+++ Test/AOJ/RollingHash.test.cpp
@@ -0,0 +1,11 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/14/ALDS1_14_B"
+
+#include <bits/stdc++.h>
+using namespace std;
+#include "../../String/RollingHash.cpp"
+typedef long long ll;
+
+int main() {
+
+	return 0;
+}
