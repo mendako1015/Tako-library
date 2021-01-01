13f30c8d202a735c095846652add97d97d7367d8
mendako1015 <h1000sunny51@icloud.com>
2021-01-02 01:00:24 +0900

    Add SparseTable test

---
+++ Test/yosupo-judge/SparseTable-StaticRMQ.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
+
+#include <bits/stdc++.h>
+using namespace std;
+#include "../../DataStructure/SparseTable.cpp"
+typedef long long ll;
+
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(false);
+
+	int n, q;
+	cin >> n >> q;
+	vector<int> a(n);
+	for(int i = 0; i < n; i++) cin >> a[i];
+	SparseTable<int> sp(a, [](int x, int y) { return min(x, y); });
+	while(q--) {
+		int l, r;
+		cin >> l >> r;
+		cout << sp.query(l, r) << endl;
+	}
+	return 0;
+}
