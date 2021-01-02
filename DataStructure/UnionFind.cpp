337d73a5724fb7e695ad6a03d0b6e676a16e1a23
mendako1015 <h1000sunny51@icloud.com>
2021-01-03 02:20:16 +0900

    updated UnionFind

--- DataStructure/UnionFind.cpp
+++ DataStructure/UnionFind.cpp
@@ -1,14 +1,16 @@
 template<class T = int>
 struct UnionFind {
 	vector<T> data;
+	vector<int> size;
 
-	UnionFind(int size): data(size, -1) {}
+	UnionFind(int n): data(n, -1), size(n, 0) {}
 
 	void unite(int x, int y) {
 		x = root(x), y = root(y);
 		if(x == y) return ;
 		if(data[x] > data[y]) swap(x, y);
 		data[x] += data[y];
+		size[x] += size[y];
 		data[y] = x;
 	}
 
@@ -19,5 +21,9 @@ struct UnionFind {
 
 	bool same(int x, int y) {
 		return root(x) == root(y);
+	}
+
+	int getsize(int x) {
+		return size[root(x)];
 	}
 };
